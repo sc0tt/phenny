8104f99a129fed834d05561fbbe16adc3c4d542f
Nick Sjostrom <nick.sjostrom12@gmail.com>
2013-08-01 15:54:05 -0400

    fixed error when .m

--- modules/mapimages.py
+++ modules/mapimages.py
@@ -1,17 +1,20 @@
 images = {}
 def m(phenny, input):
-	word = input.group(2).split()
-	if len(word) > 0:
-		key = word[0]
-		if key in images:
-			phenny.say(images[key])
-		else:
-			if len(word) > 1:
-				word = word[1]
-				images[key] = word
-				phenny.say(key + " = " + word)
+	word = input.group(2)
+	if word:
+		word = word.split()
+		if len(word) > 0:
+			key = word[0]
+			if key in images:
+				phenny.say(images[key])
 			else:
-				phenny.say("No value for key " + key)	
-
+				if len(word) > 1:
+					word = word[1]
+					images[key] = word
+					phenny.say(key + " = " + word)
+				else:
+					phenny.say("No value for key " + key)	
+	else:
+		phenny.say("-- .m <key> <value>")
 m.commands = ['m']
 m.priority = 'low'

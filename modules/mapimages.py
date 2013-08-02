b09a2310153da1e8d15a67963476f6e74fbb3388
sc0tt <scottynosabe@gmail.com>
2013-08-02 09:12:11 -0400

    Fixes tabs & length of word

--- modules/mapimages.py
+++ modules/mapimages.py
@@ -1,17 +1,17 @@
 images = {}
 def m(phenny, input):
-	word = input.group(2).split()
-	if len(word) > 0:
-		key = word[0]
-		if key in images:
-			phenny.say(images[key])
-		else:
-			if len(word) > 1:
-				word = word[1]
-				images[key] = word
-				phenny.say(key + " = " + word)
-			else:
-				phenny.say("No value for key " + key)	
+   word = input.group(2).split()
+   if len(word) > 0:
+      key = word[0]
+      if key in images:
+         phenny.say(images[key])
+      else:
+         if len(word) > 1:
+            word = word[1:]
+            images[key] = word
+            phenny.say(key + " = " + word)
+         else:
+            phenny.say("No value for key " + key)   
 
 m.commands = ['m']
 m.priority = 'low'

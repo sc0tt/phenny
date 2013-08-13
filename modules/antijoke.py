dc70e4e8bd5c13382b1b051270a782b2865508e2
sc0tt <scottynosabe@gmail.com>
2013-08-13 15:21:34 -0400

    Random pun

--- modules/antijoke.py
+++ modules/antijoke.py
@@ -1,32 +1,37 @@
 
 import feedparser
 import re
-from random import randint
+from random import randint, choice
 
 punCount = 0
 exp = re.compile('<p>(.*?)<\/p>')
 
 def antijoke(phenny, input):
-	feed = feedparser.parse('http://www.reddit.com/r/antijokes/.rss?limit=50')
-	rNum = randint(0,len(feed["items"]))
-	title = feed["items"][rNum]["title"]
-	desc = exp.findall(feed["items"][rNum]["description"])
-	phenny.say(title)
-	if len(desc) >= 1:
-		phenny.say(desc[0])
+   feed = feedparser.parse('http://www.reddit.com/r/antijokes/.rss?limit=50')
+   rNum = randint(0,len(feed["items"]))
+   title = feed["items"][rNum]["title"]
+   desc = exp.findall(feed["items"][rNum]["description"])
+   phenny.say(title)
+   if len(desc) >= 1:
+      phenny.say(desc[0])
 
 antijoke.commands = ['antijoke']
 antijoke.priority = 'low'	
 
 def pun(phenny,input):
-	puns = {}
-	feed = feedparser.parse('http://www.reddit.com/r/punny/.rss?limit=50')
-	feedlen = len(feed["items"])
-	for item in range(feedlen):
-		desc = exp.findall(feed["items"][item]["description"])
-		if len(desc) >= 1:
-			phenny.say(feed["items"][item]["title"])
-			phenny.say(desc[0])
-			break
+   feed = feedparser.parse('http://www.reddit.com/r/punny/.rss?limit=50')
+   feedlen = len(feed["items"])
+   all_puns = []
+   for item in range(feedlen):
+      desc = exp.findall(feed["items"][item]["description"])
+      if len(desc) >= 1:
+         title = feed["items"][item]["title"] 
+         txt = desc[0]
+         all_puns.append([title, txt])
+
+   pun_choice = choice(all_puns)
+   phenny.say(pun_choice[0])
+   phenny.say(pun_choice[1])
+
 pun.commands = ['pun']
 pun.priority = 'low'

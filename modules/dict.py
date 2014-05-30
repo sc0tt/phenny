4aba213af5f5f401a61989f923ef54a33b75d7af
Scott Adie <scottynosabe@gmail.com>
2014-05-30 09:35:12 -0400

    define fixes

--- modules/dict.py
+++ modules/dict.py
@@ -1,25 +1,39 @@
 import requests
 
+def lookup(word, api_key):
+  payload = {'api_key': api_key, 'limit': 1}
+  try:
+    response = requests.get("http://api.wordnik.com:80/v4/word.json/%s/definitions" % word.lower(), params=payload).json()
+    return response[0]['text']
+  except:
+    return None
 
 def define(phenny, input):
   word = input.group(2)
   if not word:
     phenny.say("-- .define <word>")
   else:
-    payload = {'api_key': phenny.config.wordnik_api_key, 'limit': 1}
-    response = requests.get("http://api.wordnik.com:80/v4/word.json/%s/definitions" % word.lower(), params=payload).json()
-    phenny.say("%s: %s" % (word, response[0]['text']))
+    definition = lookup(word, phenny.config.wordnik_api_key)
+    if definition is None:
+      phenny.say("Word not found!")
+    else:
+      phenny.say("%s: %s" % (word, definition))
 define.commands = ['define']
 
 
 def wotd(phenny, input):
   payload = {'api_key': phenny.config.wordnik_api_key}
   response = requests.get("http://api.wordnik.com:80/v4/words.json/wordOfTheDay", params=payload).json()
-  phenny.say("Word of the day: %s" % (response['word']))
+  definition = lookup(response['word'], phenny.config.wordnik_api_key)
+  phenny.say("Word of the day: %s - %s" % (response['word'], definition))
 
   
 wotd.commands = ['wotd']
 
 def rword(phenny, input):
-  pass
+  payload = {'api_key': phenny.config.wordnik_api_key,
+             'hasDictionaryDef': 'true' }
+  response = requests.get("http://api.wordnik.com:80/v4/words.json/randomWord", params=payload).json()
+  definition = lookup(response['word'], phenny.config.wordnik_api_key)
+  phenny.say("Random word: %s - %s" % (response['word'], definition))
 rword.commands = ['rword']
